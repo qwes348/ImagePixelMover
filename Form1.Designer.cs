083e3fc60ef31b06a4060501c300cdae0e7df0fc
HwangKyung <qwes348@naver.com>
2023-11-09 23:47:14 +0900

    프로젝트 업로드

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace ImagePixelMover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LoadImage = new System.Windows.Forms.Button();
+            this.savePathBox = new System.Windows.Forms.TextBox();
+            this.ImagePathLabel = new System.Windows.Forms.Label();
+            this.ImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.numericUpDown_movePixel_X = new System.Windows.Forms.NumericUpDown();
+            this.movePixelLabel_X = new System.Windows.Forms.Label();
+            this.movePixellabe_Y = new System.Windows.Forms.Label();
+            this.numericUpDown_movePixel_Y = new System.Windows.Forms.NumericUpDown();
+            this.movePixelButton = new System.Windows.Forms.Button();
+            this.savePathButton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imagePathListView = new System.Windows.Forms.ListView();
+            this.previewLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_movePixel_X)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_movePixel_Y)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadImage
+            // 
+            this.LoadImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadImage.Location = new System.Drawing.Point(469, 420);
+            this.LoadImage.Name = "LoadImage";
+            this.LoadImage.Size = new System.Drawing.Size(187, 63);
+            this.LoadImage.TabIndex = 0;
+            this.LoadImage.Text = "이미지 불러오기";
+            this.LoadImage.UseVisualStyleBackColor = true;
+            this.LoadImage.Click += new System.EventHandler(this.OnLoadImageClicked);
+            // 
+            // savePathBox
+            // 
+            this.savePathBox.Location = new System.Drawing.Point(121, 45);
+            this.savePathBox.Name = "savePathBox";
+            this.savePathBox.Size = new System.Drawing.Size(707, 21);
+            this.savePathBox.TabIndex = 1;
+            // 
+            // ImagePathLabel
+            // 
+            this.ImagePathLabel.AutoSize = true;
+            this.ImagePathLabel.Location = new System.Drawing.Point(40, 48);
+            this.ImagePathLabel.Name = "ImagePathLabel";
+            this.ImagePathLabel.Size = new System.Drawing.Size(59, 15);
+            this.ImagePathLabel.TabIndex = 2;
+            this.ImagePathLabel.Text = "저장 경로";
+            this.ImagePathLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ImagePictureBox
+            // 
+            this.ImagePictureBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.ImagePictureBox.BackColor = System.Drawing.Color.White;
+            this.ImagePictureBox.Location = new System.Drawing.Point(436, 166);
+            this.ImagePictureBox.Name = "ImagePictureBox";
+            this.ImagePictureBox.Size = new System.Drawing.Size(200, 200);
+            this.ImagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ImagePictureBox.TabIndex = 3;
+            this.ImagePictureBox.TabStop = false;
+            // 
+            // numericUpDown_movePixel_X
+            // 
+            this.numericUpDown_movePixel_X.Location = new System.Drawing.Point(316, 420);
+            this.numericUpDown_movePixel_X.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown_movePixel_X.Minimum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown_movePixel_X.Name = "numericUpDown_movePixel_X";
+            this.numericUpDown_movePixel_X.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown_movePixel_X.TabIndex = 9;
+            // 
+            // movePixelLabel_X
+            // 
+            this.movePixelLabel_X.AutoSize = true;
+            this.movePixelLabel_X.Location = new System.Drawing.Point(230, 422);
+            this.movePixelLabel_X.Name = "movePixelLabel_X";
+            this.movePixelLabel_X.Size = new System.Drawing.Size(80, 15);
+            this.movePixelLabel_X.TabIndex = 10;
+            this.movePixelLabel_X.Text = "움직일 픽셀X";
+            // 
+            // movePixellabe_Y
+            // 
+            this.movePixellabe_Y.AutoSize = true;
+            this.movePixellabe_Y.Location = new System.Drawing.Point(230, 473);
+            this.movePixellabe_Y.Name = "movePixellabe_Y";
+            this.movePixellabe_Y.Size = new System.Drawing.Size(79, 15);
+            this.movePixellabe_Y.TabIndex = 12;
+            this.movePixellabe_Y.Text = "움직일 픽셀Y";
+            // 
+            // numericUpDown_movePixel_Y
+            // 
+            this.numericUpDown_movePixel_Y.Location = new System.Drawing.Point(316, 471);
+            this.numericUpDown_movePixel_Y.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown_movePixel_Y.Minimum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown_movePixel_Y.Name = "numericUpDown_movePixel_Y";
+            this.numericUpDown_movePixel_Y.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown_movePixel_Y.TabIndex = 11;
+            // 
+            // movePixelButton
+            // 
+            this.movePixelButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.movePixelButton.Location = new System.Drawing.Point(672, 420);
+            this.movePixelButton.Name = "movePixelButton";
+            this.movePixelButton.Size = new System.Drawing.Size(188, 66);
+            this.movePixelButton.TabIndex = 13;
+            this.movePixelButton.Text = "픽셀 이동";
+            this.movePixelButton.UseVisualStyleBackColor = true;
+            this.movePixelButton.Click += new System.EventHandler(this.OnMovePixelButtonClicked);
+            // 
+            // savePathButton
+            // 
+            this.savePathButton.Location = new System.Drawing.Point(848, 33);
+            this.savePathButton.Name = "savePathButton";
+            this.savePathButton.Size = new System.Drawing.Size(98, 44);
+            this.savePathButton.TabIndex = 14;
+            this.savePathButton.Text = "저장 경로 선택";
+            this.savePathButton.UseVisualStyleBackColor = true;
+            this.savePathButton.Click += new System.EventHandler(this.SavePathButtonClicked);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imagePathListView
+            // 
+            this.imagePathListView.HideSelection = false;
+            this.imagePathListView.LargeImageList = this.imageList1;
+            this.imagePathListView.Location = new System.Drawing.Point(43, 110);
+            this.imagePathListView.Name = "imagePathListView";
+            this.imagePathListView.Size = new System.Drawing.Size(128, 393);
+            this.imagePathListView.SmallImageList = this.imageList1;
+            this.imagePathListView.TabIndex = 15;
+            this.imagePathListView.UseCompatibleStateImageBehavior = false;
+            this.imagePathListView.SelectedIndexChanged += new System.EventHandler(this.OnImagePathListItemSelected);
+            // 
+            // previewLabel
+            // 
+            this.previewLabel.AutoSize = true;
+            this.previewLabel.Location = new System.Drawing.Point(433, 131);
+            this.previewLabel.Name = "previewLabel";
+            this.previewLabel.Size = new System.Drawing.Size(59, 15);
+            this.previewLabel.TabIndex = 16;
+            this.previewLabel.Text = "크게 보기";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1006, 531);
+            this.Controls.Add(this.previewLabel);
+            this.Controls.Add(this.imagePathListView);
+            this.Controls.Add(this.savePathButton);
+            this.Controls.Add(this.movePixelButton);
+            this.Controls.Add(this.movePixellabe_Y);
+            this.Controls.Add(this.numericUpDown_movePixel_Y);
+            this.Controls.Add(this.movePixelLabel_X);
+            this.Controls.Add(this.numericUpDown_movePixel_X);
+            this.Controls.Add(this.ImagePictureBox);
+            this.Controls.Add(this.ImagePathLabel);
+            this.Controls.Add(this.savePathBox);
+            this.Controls.Add(this.LoadImage);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "픽셀 이동 툴";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_movePixel_X)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_movePixel_Y)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadImage;
+        private System.Windows.Forms.TextBox savePathBox;
+        private System.Windows.Forms.Label ImagePathLabel;
+        private System.Windows.Forms.PictureBox ImagePictureBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown_movePixel_X;
+        private System.Windows.Forms.Label movePixelLabel_X;
+        private System.Windows.Forms.Label movePixellabe_Y;
+        private System.Windows.Forms.NumericUpDown numericUpDown_movePixel_Y;
+        private System.Windows.Forms.Button movePixelButton;
+        private System.Windows.Forms.Button savePathButton;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView imagePathListView;
+        private System.Windows.Forms.Label previewLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ImagePixelMover
+{
+    public partial class Form1 : Form
+    {        
+        private List<string> selectedFileNames;
+        private string savePath;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 초기 저장 위치는 바탕화면 path
+            savePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            savePathBox.Text = savePath;
+        }
+
+        private void OnLoadImageClicked(object sender, EventArgs e)
+        {
+            // 파일 선택창을 띄움
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.IsFolderPicker = false;
+            // 파일선택 완료 시 다음 함수로 전달
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                OnFilesSelectComplete(dialog);
+            }
+        }
+
+        private void OnFilesSelectComplete(CommonOpenFileDialog dialog)
+        {            
+            Image img = null;
+            imagePathListView.Clear();
+            imagePathListView.View = View.List;
+
+            try
+            {
+                selectedFileNames = dialog.FileNames.ToList();
+                if (selectedFileNames == null || selectedFileNames.Count <= 0)
+                    throw new ArgumentException("선택된 파일이 없습니다.");
+
+                for (int i = 0; i < selectedFileNames.Count; i++)
+                {
+                    imageList1.Images.Add(Image.FromFile(selectedFileNames[i]));
+                }
+
+                // 첫번째 이미지만 불러오기
+                img = Image.FromFile(selectedFileNames[0]);
+
+                // 이미지 객체를 복사해서 PictureBox에 넣음
+                ImagePictureBox.Image = (Image)img.Clone();
+                
+                for (int i = 0; i < selectedFileNames.Count; i++)
+                {
+                    ListViewItem item = new ListViewItem(selectedFileNames[i], i);
+                    imagePathListView.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                img.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 이미지 주소 리스트에서 새로운 이미지가 선택됐을 때
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnImagePathListItemSelected(object sender, EventArgs e)
+        {
+            if (imagePathListView.SelectedItems.Count <= 0)
+                return;
+            string path = imagePathListView.SelectedItems[0].Text;
+            Image img = null;
+
+            try
+            {
+                img = Image.FromFile(path);
+
+                // 이미지 객체를 복사해서 PictureBox에 넣음
+                ImagePictureBox.Image = (Image)img.Clone();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (img != null)
+                    img.Dispose();
+            }
+        }
+
+        private void OnMovePixelButtonClicked(object sender, EventArgs e)
+        {
+            if(selectedFileNames == null || selectedFileNames.Count <= 0)
+            {
+                MessageBox.Show("이미지를 먼저 불러와주세요", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int moveX = (int)numericUpDown_movePixel_X.Value;
+            // y축은 확인해보니 양수면 아래로 보내고 음수면 위로 보냄 따라서 반전시켜줌
+            int moveY = (int)numericUpDown_movePixel_Y.Value * -1;
+
+            for (int i = 0; i < selectedFileNames.Count; i++)
+            {
+                string path = selectedFileNames[i];
+
+                Bitmap originBmp = new Bitmap(path);
+                Bitmap newBmp = new Bitmap(path);
+                // 모든 픽셀을 돌면서 이동시킴
+                for (int y = 0; y < originBmp.Height; y++)
+                {
+                    for (int x = 0; x < originBmp.Width; x++)
+                    {
+                        // 범위를 초과한 경우 continue
+                        if ((y + moveY) >= originBmp.Height)
+                            continue;
+                        if ((y + moveY) < 0)
+                            continue;
+                        if ((x + moveX) >= originBmp.Width)
+                            continue;
+                        if ((x + moveX) < 0)
+                            continue;
+
+                        newBmp.SetPixel(x + moveX, y + moveY, originBmp.GetPixel(x, y));
+                    }
+                }
+
+                string fileName = Path.GetFileNameWithoutExtension(path);
+                newBmp.Save(Path.Combine(savePath, fileName) + ".png", ImageFormat.Png);
+            }
+        }
+
+        private void SavePathButtonClicked(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            // 파일선택 완료 시 다음 함수로 전달
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                savePath = dialog.FileName;
+                savePathBox.Text = savePath;
+            }
+        }
+    }
+}
